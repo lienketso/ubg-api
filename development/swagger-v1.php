79c590bb4f64b1268a298254e18ba5666289f55e
Nguyen Thanh An <thanhan1507@gmail.com>
2022-04-25 10:14:31 +0700

    base url image

--- development/swagger-v1.php
+++ development/swagger-v1.php
@@ -7,7 +7,7 @@
  *     @SWG\Info(
  *         version="1.0.0",
  *         title="Laravel and Swagger",
- *         description="Getting started with Laravel and Swagger",
+ *         description="base url image https://ubgmart.com/storage/ , sử dụng basic auth theo tài khoản được cung cấp để truy cập api",
  *         termsOfService="",
  *         @SWG\Contact(
  *             email="thanhan1507@gmail.com"
