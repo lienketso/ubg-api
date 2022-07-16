fa5464fa0e77812214055a24b5f1457eb10a2447
Long Le Ngoc <longln@tohsoft.com>
2022-07-16 11:25:43 +0700

    update

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -22,6 +22,7 @@ class AuthController extends Controller
     protected $customerRepository;
     protected $ubgxuPaylogRepository;
     protected $settingRegisterRepository;
+
     public function __construct(
         CustomerRepository $customerRepository,
         UbgxuPaylogRepository $ubgxuPaylogRepository,
@@ -31,6 +32,7 @@ class AuthController extends Controller
         $this->customerRepository = $customerRepository;
         $this->ubgxuPaylogRepository = $ubgxuPaylogRepository;
     }
+
     /**
      * @SWG\Post(
      *     path="/api/auth/register",
@@ -76,42 +78,51 @@ class AuthController extends Controller
      *     )
      * )
      */
-    public function register(Request $request){
+    public function register(Request $request)
+    {
 //        $settingRegister = SettingRegister::where('type','app')
 //            ->where('expire_date','>=',now()->toDateString())
 //            ->where('start_date','>=',now()->toDateString())
 //            ->first();
-        if(is_numeric($request->username)){
+        if (is_numeric($request->username)) {
             $validator = Validator::make($request->all(), [
-                'username'=>'required|numeric|unique:ec_customers,phone',
+                'username' => 'required|numeric|unique:ec_customers,phone',
                 'password' => 'required|string|confirmed|min:6',
-            ]);
-            if($validator->fails()){
-                return response()->json($validator->errors());
-            }
-            $customer = Customers::create([
-                'phone'=>$request->username,
-                'password'=>Hash::make($request->password),
-                'name'=>'User default',
-                'register_resource'=>'app'
             ]);
 
-            }else if(filter_var($request->username, FILTER_VALIDATE_EMAIL)){
+            if ($validator->fails()) {
+                return response()->json($validator->errors());
+            }
+
+            $customer = Customers::create([
+                'phone' => $request->username,
+                'password' => Hash::make($request->password),
+                'name' => 'User default',
+                'register_resource' => 'app'
+            ]);
+
+            $customer->affiliation_id = intval(1000000 + $customer->id);
+            $customer->save();
+
+        } else if (filter_var($request->username, FILTER_VALIDATE_EMAIL)) {
             $validator = Validator::make($request->all(), [
-                'username'=>'required|email|unique:ec_customers,email',
+                'username' => 'required|email|unique:ec_customers,email',
                 'password' => 'required|string|confirmed|min:6',
             ]);
-            if($validator->fails()){
+            if ($validator->fails()) {
                 return response()->json($validator->errors());
             }
             $customer = Customers::create([
-                'email'=>$request->username,
-                'password'=>Hash::make($request->password),
-                'name'=>'User default',
-                'register_resource'=>'app',
+                'email' => $request->username,
+                'password' => Hash::make($request->password),
+                'name' => 'User default',
+                'register_resource' => 'app',
                 'is_verified' => 1,
                 'status' => 'active'
             ]);
+
+            $customer->affiliation_id = intval(1000000 + $customer->id);
+            $customer->save();
         }
         //nếu có chương trình tặng xu
 //        if($settingRegister && $settingRegister!=null){
@@ -127,29 +138,29 @@ class AuthController extends Controller
 //        }
 
         //nếu có mã giới thiệu
-        if($request->input('affiliation_id') != null){
-            $presenterUser = $this->customerRepository->findWhere(['affiliation_id'=>$request->input('affiliation_id')])->first();
-            $aff = ['presenter_id'=>$presenterUser->id];
-            $this->customerRepository->update($aff,$customer->id);
+        if ($request->input('affiliation_id') != null) {
+            $presenterUser = $this->customerRepository->findWhere(['affiliation_id' => $request->input('affiliation_id')])->first();
+            $aff = ['presenter_id' => $presenterUser->id];
+            $this->customerRepository->update($aff, $customer->id);
         }
 
 
         $token = $customer->createToken('auth_token')->plainTextToken;
 
         return response()
-            ->json(['data' => $customer,'access_token' => $token, 'token_type' => 'Bearer']);
+            ->json(['data' => $customer, 'access_token' => $token, 'token_type' => 'Bearer']);
     }
 
-    protected function credentials($username,$password)
+    protected function credentials($username, $password)
     {
-        if(is_numeric($username)){
-            return ['phone'=>$username,'password'=>$password];
+        if (is_numeric($username)) {
+            return ['phone' => $username, 'password' => $password];
+        } elseif (filter_var($username, FILTER_VALIDATE_EMAIL)) {
+            return ['email' => $username, 'password' => $password];
         }
-        elseif (filter_var($username, FILTER_VALIDATE_EMAIL)) {
-            return ['email' => $username, 'password'=>$password];
-        }
-        return ['phone' => $username, 'password'=>$password];
+        return ['phone' => $username, 'password' => $password];
     }
+
     /**
      * @SWG\Post(
      *     path="/api/auth/login",
@@ -181,18 +192,19 @@ class AuthController extends Controller
      *     )
      * )
      */
-    public function login(Request $request){
-        $validator = Validator::make($request->all(),[
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'username' => 'required',
             'password' => 'required'
         ]);
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors());
         }
 
 //        $credentials = request(['phone', 'password']);
-        $credentials = $this->credentials($request->username,$request->password);
-        if(!Auth::attempt($credentials)) {
+        $credentials = $this->credentials($request->username, $request->password);
+        if (!Auth::attempt($credentials)) {
             return response()->json([
                 'message' => 'Unauthorized'
             ], 401);
@@ -200,7 +212,7 @@ class AuthController extends Controller
         $user = $request->user();
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
-        if ($request->remember_me){
+        if ($request->remember_me) {
             $token->expires_at = Carbon::now()->addWeeks(1);
         }
 
@@ -211,9 +223,10 @@ class AuthController extends Controller
             'expires_at' => Carbon::parse(
                 $tokenResult->token->expires_at
             )->toDateTimeString(),
-            'message'=>'Login success !'
+            'message' => 'Login success !'
         ]);
     }
+
     /**
      * @SWG\Get(
      *     path="/api/auth/logout",
@@ -230,12 +243,14 @@ class AuthController extends Controller
      *     )
      * )
      */
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         $request->user()->token()->revoke();
         return response()->json([
             'message' => 'Successfully logged out'
         ]);
     }
+
     /**
      * @SWG\Get(
      *     path="/api/auth/user",
@@ -263,6 +278,7 @@ class AuthController extends Controller
     {
         return response()->json($request->user());
     }
+
     /**
      * @SWG\Post(
      *     path="/api/auth/update-user",
@@ -315,11 +331,13 @@ class AuthController extends Controller
      *     )
      * )
      */
-    public function updateProfile(Request $request){
+    public function updateProfile(Request $request)
+    {
         $customer = Customers::find($request->id);
         $customer->update($request->all());
         return response()->json($customer);
     }
+
     /**
      * @SWG\Post(
      *     path="/api/auth/reset-password",
@@ -351,47 +369,50 @@ class AuthController extends Controller
      *     )
      * )
      */
-    public function ForgotPassword(Request $request){
+    public function ForgotPassword(Request $request)
+    {
         $username = $request->username;
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'username' => 'required',
             'password' => 'required|string|min:6'
         ]);
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors());
         }
 
-            $info = $this->customerRepository->scopeQuery(function ($e) use ($username){
-                return $e->where('phone',$username)->orWhere('email',$username);
-            })->first();
+        $info = $this->customerRepository->scopeQuery(function ($e) use ($username) {
+            return $e->where('phone', $username)->orWhere('email', $username);
+        })->first();
 
-            if($info){
-                $data = [
-                  'password'=>Hash::make($request->password)
-                ];
-                $this->customerRepository->update($data,$info->id);
-                return response()->json(['message'=>'Change password successful !']);
-            }else{
-                return response()->json(['error'=>'User not found !']);
-            }
+        if ($info) {
+            $data = [
+                'password' => Hash::make($request->password)
+            ];
+            $this->customerRepository->update($data, $info->id);
+            return response()->json(['message' => 'Change password successful !']);
+        } else {
+            return response()->json(['error' => 'User not found !']);
+        }
 
     }
 
-    public function LoginWithGoogle($driver,Request $request){
-            $user = Socialite::driver($driver)->user();
-            $existingUser = Customers::where('email',$user->getEmail())->first();
-            if($existingUser){
-                auth()->login($existingUser,true);
-            }else{
-                $newUser = new Customers;
-                $newUser->name = $user->getName();
-                $newUser->email = $user->getEmail();
-                $newUser->confirmed_at = now();
-                $newUser->avatar = $user->getAvatar();
-                $newUser->save();
-                auth()->login($newUser, true);
-            }
+    public function LoginWithGoogle($driver, Request $request)
+    {
+        $user = Socialite::driver($driver)->user();
+        $existingUser = Customers::where('email', $user->getEmail())->first();
+        if ($existingUser) {
+            auth()->login($existingUser, true);
+        } else {
+            $newUser = new Customers;
+            $newUser->name = $user->getName();
+            $newUser->email = $user->getEmail();
+            $newUser->confirmed_at = now();
+            $newUser->avatar = $user->getAvatar();
+            $newUser->save();
+            auth()->login($newUser, true);
+        }
     }
+
     /**
      * @SWG\Get(
      *     path="/api/auth/get-presenter-by-me",
@@ -415,25 +436,27 @@ class AuthController extends Controller
      *     )
      * )
      */
-    public function getPresenterByAuth(Request $request){
+    public function getPresenterByAuth(Request $request)
+    {
 
-        if(!$request->user()){
-            return response()->json(['error'=>'Authentication fail']);
+        if (!$request->user()) {
+            return response()->json(['error' => 'Authentication fail']);
         }
-        try{
+        try {
             $currentUserId = $request->user()->id;
             $listPresenter = $this->customerRepository->getModel()
-                ->where('is_verified',1)
+                ->where('is_verified', 1)
                 ->whereNull('is_affiliater')
-                ->where('presenter_id',$currentUserId)
+                ->where('presenter_id', $currentUserId)
                 ->get();
             return response()->json($listPresenter);
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             return $e->getMessage();
         }
 
 
     }
+
     /**
      * @SWG\Get(
      *     path="/api/auth/permanent-delete",
@@ -451,7 +474,8 @@ class AuthController extends Controller
      *     )
      * )
      */
-    public function getPermanentDelete(){
+    public function getPermanentDelete()
+    {
         return response('Success permanent delete ', 200)
             ->header('Content-Type', 'text/plain');
     }
